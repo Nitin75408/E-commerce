73740a15c0a7ae816a22a54dd471f2b2d092f7c8
NitinMadan001 <nitinmad75@gmail.com>
2025-06-14 14:43:25 +0530

    Adding copywrite

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,6 +1,9 @@
+
 import React from "react";
 import { assets } from "@/assets/assets";
 import Image from "next/image";
+import { useRouter } from "next/router";
+
 
 const Banner = () => {
   return (
